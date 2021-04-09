c5c42a568d63ae06347a4e702fed441ec14c3e61
CaptainDario <51273483+CaptainDario@users.noreply.github.com>
2021-04-09 12:51:37 +0200

    Update get_play_reviews.py

--- src/get_play_reviews.py
+++ src/get_play_reviews.py
@@ -1,7 +1,7 @@
 from google_play_scraper import app
 
 result = app(
-    'com.nianticlabs.pokemongo',
+    'com.daapplab.dakanjirecognizer',
     lang='en', # defaults to 'en'
     country='us' # defaults to 'us'
 )
